3cf29f4c37f03e0d46e13a08e22ca1d15851936c
Andreas Dorrer <andreas.dorrer@a1.net>
2024-07-19 21:52:25 +0200

    format

--- lcd-routines.c
+++ lcd-routines.c
@@ -1,170 +1,170 @@
 #include <avr/io.h>
 #include "lcd-routines.h"
 #include <util/delay.h>
- 
+
 ////////////////////////////////////////////////////////////////////////////////
 // Erzeugt einen Enable-Puls
-static void lcd_enable( void )
+static void lcd_enable(void)
 {
-    LCD_PORT |= (1<<LCD_EN);     // Enable auf 1 setzen
-    _delay_us( LCD_ENABLE_US );  // kurze Pause
-    LCD_PORT &= ~(1<<LCD_EN);    // Enable auf 0 setzen
+    LCD_PORT |= (1 << LCD_EN);  // Enable auf 1 setzen
+    _delay_us(LCD_ENABLE_US);   // kurze Pause
+    LCD_PORT &= ~(1 << LCD_EN); // Enable auf 0 setzen
 }
- 
+
 ////////////////////////////////////////////////////////////////////////////////
 // Sendet eine 4-bit Ausgabeoperation an das LCD
-static void lcd_out( uint8_t data )
+static void lcd_out(uint8_t data)
 {
-    data &= 0xF0;                       // obere 4 Bit maskieren
- 
-    LCD_PORT &= ~(0xF0>>(4-LCD_DB));    // Maske loeschen
-    LCD_PORT |= (data>>(4-LCD_DB));     // Bits setzen
+    data &= 0xF0; // obere 4 Bit maskieren
+
+    LCD_PORT &= ~(0xF0 >> (4 - LCD_DB)); // Maske loeschen
+    LCD_PORT |= (data >> (4 - LCD_DB));  // Bits setzen
     lcd_enable();
 }
- 
+
 ////////////////////////////////////////////////////////////////////////////////
 // Initialisierung: muss ganz am Anfang des Programms aufgerufen werden.
-void lcd_init( void )
+void lcd_init(void)
 {
     // verwendete Pins auf Ausgang schalten
-    uint8_t pins = (0x0F << LCD_DB) |           // 4 Datenleitungen
-                   (1<<LCD_RS) |                // R/S Leitung
-                   (1<<LCD_EN);                 // Enable Leitung
+    uint8_t pins = (0x0F << LCD_DB) | // 4 Datenleitungen
+                   (1 << LCD_RS) |    // R/S Leitung
+                   (1 << LCD_EN);     // Enable Leitung
     LCD_DDR |= pins;
- 
+
     // initial alle Ausgaenge auf Null
     LCD_PORT &= ~pins;
- 
+
     // warten auf die Bereitschaft des LCD
-    _delay_ms( LCD_BOOTUP_MS );
-    
+    _delay_ms(LCD_BOOTUP_MS);
+
     // Soft-Reset muss 3mal hintereinander gesendet werden zur Initialisierung
-    lcd_out( LCD_SOFT_RESET );
-    _delay_ms( LCD_SOFT_RESET_MS1 );
- 
+    lcd_out(LCD_SOFT_RESET);
+    _delay_ms(LCD_SOFT_RESET_MS1);
+
     lcd_enable();
-    _delay_ms( LCD_SOFT_RESET_MS2 );
- 
+    _delay_ms(LCD_SOFT_RESET_MS2);
+
     lcd_enable();
-    _delay_ms( LCD_SOFT_RESET_MS3 );
- 
-    // 4-bit Modus aktivieren 
-    lcd_out( LCD_SET_FUNCTION |
-             LCD_FUNCTION_4BIT );
-    _delay_ms( LCD_SET_4BITMODE_MS );
- 
+    _delay_ms(LCD_SOFT_RESET_MS3);
+
+    // 4-bit Modus aktivieren
+    lcd_out(LCD_SET_FUNCTION |
+            LCD_FUNCTION_4BIT);
+    _delay_ms(LCD_SET_4BITMODE_MS);
+
     // 4-bit Modus / 2 Zeilen / 5x7
-    lcd_command( LCD_SET_FUNCTION |
-                 LCD_FUNCTION_4BIT |
-                 LCD_FUNCTION_2LINE |
-                 LCD_FUNCTION_5X7 );
- 
+    lcd_command(LCD_SET_FUNCTION |
+                LCD_FUNCTION_4BIT |
+                LCD_FUNCTION_2LINE |
+                LCD_FUNCTION_5X7);
+
     // Display ein / Cursor aus / Blinken aus
-    lcd_command( LCD_SET_DISPLAY |
-                 LCD_DISPLAY_ON |
-                 LCD_CURSOR_OFF |
-                 LCD_BLINKING_OFF); 
- 
+    lcd_command(LCD_SET_DISPLAY |
+                LCD_DISPLAY_ON |
+                LCD_CURSOR_OFF |
+                LCD_BLINKING_OFF);
+
     // Cursor inkrement / kein Scrollen
-    lcd_command( LCD_SET_ENTRY |
-                 LCD_ENTRY_INCREASE |
-                 LCD_ENTRY_NOSHIFT );
- 
+    lcd_command(LCD_SET_ENTRY |
+                LCD_ENTRY_INCREASE |
+                LCD_ENTRY_NOSHIFT);
+
     lcd_clear();
 }
-  
+
 ////////////////////////////////////////////////////////////////////////////////
 // Sendet ein Datenbyte an das LCD
-void lcd_data( uint8_t data )
+void lcd_data(uint8_t data)
 {
-    LCD_PORT |= (1<<LCD_RS);    // RS auf 1 setzen
- 
-    lcd_out( data );            // zuerst die oberen, 
-    lcd_out( data<<4 );         // dann die unteren 4 Bit senden
- 
-    _delay_us( LCD_WRITEDATA_US );
+    LCD_PORT |= (1 << LCD_RS); // RS auf 1 setzen
+
+    lcd_out(data);      // zuerst die oberen,
+    lcd_out(data << 4); // dann die unteren 4 Bit senden
+
+    _delay_us(LCD_WRITEDATA_US);
 }
- 
+
 ////////////////////////////////////////////////////////////////////////////////
 // Sendet einen Befehl an das LCD
-void lcd_command( uint8_t data )
+void lcd_command(uint8_t data)
 {
-    LCD_PORT &= ~(1<<LCD_RS);    // RS auf 0 setzen
- 
-    lcd_out( data );             // zuerst die oberen, 
-    lcd_out( data<<4 );           // dann die unteren 4 Bit senden
- 
-    _delay_us( LCD_COMMAND_US );
+    LCD_PORT &= ~(1 << LCD_RS); // RS auf 0 setzen
+
+    lcd_out(data);      // zuerst die oberen,
+    lcd_out(data << 4); // dann die unteren 4 Bit senden
+
+    _delay_us(LCD_COMMAND_US);
 }
- 
+
 ////////////////////////////////////////////////////////////////////////////////
 // Sendet den Befehl zur Loeschung des Displays
-void lcd_clear( void )
+void lcd_clear(void)
 {
-    lcd_command( LCD_CLEAR_DISPLAY );
-    _delay_ms( LCD_CLEAR_DISPLAY_MS );
+    lcd_command(LCD_CLEAR_DISPLAY);
+    _delay_ms(LCD_CLEAR_DISPLAY_MS);
 }
- 
+
 ////////////////////////////////////////////////////////////////////////////////
 // Sendet den Befehl: Cursor Home
-void lcd_home( void )
+void lcd_home(void)
 {
-    lcd_command( LCD_CURSOR_HOME );
-    _delay_ms( LCD_CURSOR_HOME_MS );
+    lcd_command(LCD_CURSOR_HOME);
+    _delay_ms(LCD_CURSOR_HOME_MS);
 }
- 
+
 ////////////////////////////////////////////////////////////////////////////////
-// Setzt den Cursor in Spalte x (0..15) Zeile y (1..4) 
- 
-void lcd_setcursor( uint8_t x, uint8_t y )
+// Setzt den Cursor in Spalte x (0..15) Zeile y (1..4)
+
+void lcd_setcursor(uint8_t x, uint8_t y)
 {
     uint8_t data;
- 
+
     switch (y)
     {
-        case 1:    // 1. Zeile
-            data = LCD_SET_DDADR + LCD_DDADR_LINE1 + x;
-            break;
- 
-        case 2:    // 2. Zeile
-            data = LCD_SET_DDADR + LCD_DDADR_LINE2 + x;
-            break;
- 
-        case 3:    // 3. Zeile
-            data = LCD_SET_DDADR + LCD_DDADR_LINE3 + x;
-            break;
- 
-        case 4:    // 4. Zeile
-            data = LCD_SET_DDADR + LCD_DDADR_LINE4 + x;
-            break;
- 
-        default:
-            return;                                   // fuer den Fall einer falschen Zeile
+    case 1: // 1. Zeile
+        data = LCD_SET_DDADR + LCD_DDADR_LINE1 + x;
+        break;
+
+    case 2: // 2. Zeile
+        data = LCD_SET_DDADR + LCD_DDADR_LINE2 + x;
+        break;
+
+    case 3: // 3. Zeile
+        data = LCD_SET_DDADR + LCD_DDADR_LINE3 + x;
+        break;
+
+    case 4: // 4. Zeile
+        data = LCD_SET_DDADR + LCD_DDADR_LINE4 + x;
+        break;
+
+    default:
+        return; // fuer den Fall einer falschen Zeile
     }
- 
-    lcd_command( data );
+
+    lcd_command(data);
 }
- 
+
 ////////////////////////////////////////////////////////////////////////////////
 // Schreibt einen String auf das LCD
- 
-void lcd_string( const char *data )
+
+void lcd_string(const char *data)
 {
-    while( *data != '\0' )
-        lcd_data( *data++ );
+    while (*data != '\0')
+        lcd_data(*data++);
 }
- 
+
 ////////////////////////////////////////////////////////////////////////////////
 // Schreibt ein Zeichen in den Character Generator RAM
- 
-void lcd_generatechar( uint8_t code, const uint8_t *data )
+
+void lcd_generatechar(uint8_t code, const uint8_t *data)
 {
     // Startposition des Zeichens einstellen
-    lcd_command( LCD_SET_CGADR | (code<<3) );
- 
+    lcd_command(LCD_SET_CGADR | (code << 3));
+
     // Bitmuster uebertragen
-    for ( uint8_t i=0; i<8; i++ )
+    for (uint8_t i = 0; i < 8; i++)
     {
-        lcd_data( data[i] );
+        lcd_data(data[i]);
     }
 }
--- main.c
+++ main.c
@@ -7,20 +7,21 @@
 #include <avr/eeprom.h>
 
 // define temp sensor pin
-#define W1_PIN	PD6
-#define W1_IN	PIND
-#define W1_OUT	PORTD
-#define W1_DDR	DDRD
+#define W1_PIN PD6
+#define W1_IN PIND
+#define W1_OUT PORTD
+#define W1_DDR DDRD
 
-volatile float Tist=0;
-volatile float Tsoll=8.0;
-volatile int timer0=0;
-volatile char tempH,tempL,hilf;
+volatile float Tist = 0;
+volatile float Tsoll = 8.0;
+volatile int timer0 = 0;
+volatile char tempH, tempL, hilf;
 volatile int sleep = 1, iMode = 1; // mode: 1 = gas, 2 = DC, 3 = AC
 volatile int iLights = 1, iLightsCount = 0, iResetLights = 0, iBuzzer = 0, iBuzzerCount = 0;
 int iIsOn = 0;
-float fInVoltage = 0, Tsoll_old = 0;;
-void myPrintf(float fVal, char*);
+float fInVoltage = 0, Tsoll_old = 0;
+;
+void myPrintf(float fVal, char *);
 void updateLCD(int iLowBat);
 
 int main()
@@ -28,138 +29,140 @@ int main()
 	float Tsoll_round;
 	float fSpgFlam = 0;
 	int iLowBat = 0;
-	
+
 	// ext INT0
 	// rising edge on INT0
-	MCUCR |= (1<<ISC01) | (1<<ISC00);  
-	// global interrupt flag for INT0 
-	GICR  |= (1<<INT0);		
+	MCUCR |= (1 << ISC01) | (1 << ISC00);
+	// global interrupt flag for INT0
+	GICR |= (1 << INT0);
 	// internal pull up for INT0 pin
-	PORTD |= (1<<DDD2);				
+	PORTD |= (1 << DDD2);
 
 	// Timer 0
 	// start timer 0 with prescaler 1024
-	TCCR0 |= (1<<CS02)|(1<<CS00);     
-	// enable timer 0 overflow interrupt	
-	TIMSK |= (1<<TOIE0);                
-	
+	TCCR0 |= (1 << CS02) | (1 << CS00);
+	// enable timer 0 overflow interrupt
+	TIMSK |= (1 << TOIE0);
+
 	// Timer 1
 	// start timer 1 with prescaler 256
-	TCCR1B |= (1<<CS11);       	
-	TCCR1A |= (1<<WGM10);
+	TCCR1B |= (1 << CS11);
+	TCCR1A |= (1 << WGM10);
 	// enable timer 1 overflow interrupt
-	TIMSK |= (1<<TOIE1);        
-	
+	TIMSK |= (1 << TOIE1);
+
 	// ADC
 	// Set reference to AVCC and input to ADC0
-	ADMUX = (1<<REFS0);        	
+	ADMUX = (1 << REFS0);
 	// Enable ADC, set prescaler to 128 // Fadc=Fcpu/prescaler=1000000/16=62.5kHz
-	ADCSRA = ((1<<ADEN)|(1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0));   
-	
+	ADCSRA = ((1 << ADEN) | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0));
+
 	// enable interrupts
-	sei();					
+	sei();
 
 	// Start the first conversion
-	ADCSRA |= (1<<ADSC);              			
-	
-	DDRD |= (1<<PD0);	// 230V relay
-	DDRD |= (1<<PD1);	// 12V relay
-	DDRD |= (1<<PD4);   // buzzer
-	DDRD |= (1<<PD7);	// light
-	PORTD &= ~(1<<PD0); // off
-	PORTD &= ~(1<<PD1); // off
-	PORTD &= ~(1<<PD4); // off
-	PORTD|=(1<<PD7);
+	ADCSRA |= (1 << ADSC);
+
+	DDRD |= (1 << PD0);	  // 230V relay
+	DDRD |= (1 << PD1);	  // 12V relay
+	DDRD |= (1 << PD4);	  // buzzer
+	DDRD |= (1 << PD7);	  // light
+	PORTD &= ~(1 << PD0); // off
+	PORTD &= ~(1 << PD1); // off
+	PORTD &= ~(1 << PD4); // off
+	PORTD |= (1 << PD7);
 
 	lcd_init();
 	lcd_string("Pflanzbeet Braeu");
-	lcd_setcursor(1,2);
+	lcd_setcursor(1, 2);
 	lcd_string("Andreas Dorrer");
-	iBuzzer  = 1;
+	iBuzzer = 1;
 	_delay_ms(1000);
 	iBuzzer = 0;
-	PORTD &= ~(1<<PD4); // Buzzer off
+	PORTD &= ~(1 << PD4); // Buzzer off
 	_delay_ms(2000);
 	lcd_clear();
-		
+
 	wdt_enable(WDTO_1S);
 	iLightsCount = 0;
-	while(1)
-	{			
-		ADMUX = (1<<REFS0);// ((1<<REFS0) | (1<<REFS0));
-		ADCSRA |= (1<<ADSC);
-		while(ADCSRA &(1<<ADSC));
-		
-		Tsoll = (ADC * 5.0/1024.0) * 5;		
-		Tsoll_round = (int) Tsoll;
-		
-		if(Tsoll - Tsoll_round > 0.5)
+	while (1)
+	{
+		ADMUX = (1 << REFS0); // ((1<<REFS0) | (1<<REFS0));
+		ADCSRA |= (1 << ADSC);
+		while (ADCSRA & (1 << ADSC))
+			;
+
+		Tsoll = (ADC * 5.0 / 1024.0) * 5;
+		Tsoll_round = (int)Tsoll;
+
+		if (Tsoll - Tsoll_round > 0.5)
 			Tsoll = Tsoll_round + 0.5;
 		else
 			Tsoll = Tsoll_round;
-			
-		if(Tsoll_old > Tsoll + 0.5 || Tsoll_old < Tsoll - 0.5)
+
+		if (Tsoll_old > Tsoll + 0.5 || Tsoll_old < Tsoll - 0.5)
 			iLights = 1;
-		
-		if(Tsoll_old != Tsoll && iLights == 1)
+
+		if (Tsoll_old != Tsoll && iLights == 1)
 		{
 			Tsoll_old = Tsoll;
-			iResetLights = 1;		
+			iResetLights = 1;
 			updateLCD(iLowBat);
-		}			
-				
-		if(iLights == 1)
-			PORTD|=(1<<PD7);
+		}
+
+		if (iLights == 1)
+			PORTD |= (1 << PD7);
 		else
-			PORTD &= ~(1<<PD7);
-					
-		if(sleep == 1)
+			PORTD &= ~(1 << PD7);
+
+		if (sleep == 1)
 		{
-			sleep = 0;	
-			if(tempL > 0x80)
+			sleep = 0;
+			if (tempL > 0x80)
 			{
-				hilf=~tempH;
-				Tist=(float)(hilf/(-2));
-			}	
+				hilf = ~tempH;
+				Tist = (float)(hilf / (-2));
+			}
 			else
-				Tist=(float)tempH/2;
+				Tist = (float)tempH / 2;
 
 			//// Temp Controller ////////////////////////////////
-			if(iMode != 1)
-			{			
-				if(Tist <= (Tsoll_old - 1) || iLowBat == 1)
+			if (iMode != 1)
+			{
+				if (Tist <= (Tsoll_old - 1) || iLowBat == 1)
 				{
-					if(iMode == 2)
-						PORTD &= ~(1<<PD1);		// AUS DC
-					else if(iMode == 3)
-						PORTD &= ~(1<<PD0);		// AUS AC
+					if (iMode == 2)
+						PORTD &= ~(1 << PD1); // AUS DC
+					else if (iMode == 3)
+						PORTD &= ~(1 << PD0); // AUS AC
 					iIsOn = 0;
-				}						
-				else if(Tist >= (Tsoll_old + 0.5) && iLowBat == 0)
+				}
+				else if (Tist >= (Tsoll_old + 0.5) && iLowBat == 0)
 				{
-					if(iMode == 2)
-						PORTD|=(1<<PD1);		// EIN DC
-					else if(iMode == 3)
-						PORTD|=(1<<PD0);		// EIN AC
+					if (iMode == 2)
+						PORTD |= (1 << PD1); // EIN DC
+					else if (iMode == 3)
+						PORTD |= (1 << PD0); // EIN AC
 					iIsOn = 1;
 				}
 			}
-			
+
 			//// Input Voltage Check ////////////////////////////
-			if(iMode != 3)
+			if (iMode != 3)
 			{
-				ADMUX = (1<<REFS0)|(1<<MUX0);
-				ADCSRA |= (1<<ADSC);
-				while(ADCSRA &(1<<ADSC));
-			
-				fInVoltage = (ADC * 5.0/1024.0) / 0.31122;
-				
-				if(fInVoltage < 10.3 && iMode == 1)
-				{	
+				ADMUX = (1 << REFS0) | (1 << MUX0);
+				ADCSRA |= (1 << ADSC);
+				while (ADCSRA & (1 << ADSC))
+					;
+
+				fInVoltage = (ADC * 5.0 / 1024.0) / 0.31122;
+
+				if (fInVoltage < 10.3 && iMode == 1)
+				{
 					iLowBat = 1;
 					iBuzzer = 1;
 				}
-				else if(fInVoltage < 10.9 && iMode == 2)
+				else if (fInVoltage < 10.9 && iMode == 2)
 				{
 					iLowBat = 1;
 					iBuzzer = 1;
@@ -167,58 +170,59 @@ int main()
 				else
 				{
 					iLowBat = 0;
-					if(iMode == 2 && iIsOn != 0)
+					if (iMode == 2 && iIsOn != 0)
 						iBuzzer = 0;
-					else if(iMode != 2)
-					    iBuzzer = 0;
-				}								
+					else if (iMode != 2)
+						iBuzzer = 0;
+				}
 			}
 			else
-			{	
+			{
 				iLowBat = 0;
 				iBuzzer = 0;
-			}		
-			
+			}
+
 			//// Flame observation //////////////////////////////
-			if(iMode == 1)
+			if (iMode == 1)
 			{
-				ADMUX = (1<<REFS0)|(1<<MUX1);
-				ADCSRA |= (1<<ADSC);
-				while(ADCSRA &(1<<ADSC));
-				
-				fSpgFlam = ADC * 5.0/1024.0;	
-				if(fSpgFlam > 2.0)
-				{	
+				ADMUX = (1 << REFS0) | (1 << MUX1);
+				ADCSRA |= (1 << ADSC);
+				while (ADCSRA & (1 << ADSC))
+					;
+
+				fSpgFlam = ADC * 5.0 / 1024.0;
+				if (fSpgFlam > 2.0)
+				{
 					iBuzzer = 1;
 					iIsOn = 0;
 				}
 				else
-				{	
-					if(iLowBat != 1)
+				{
+					if (iLowBat != 1)
 						iBuzzer = 0;
 					iIsOn = 1;
 				}
 			}
 
-			updateLCD(iLowBat);				
+			updateLCD(iLowBat);
 		}
-		wdt_reset();		
+		wdt_reset();
 	}
 	return 0;
 }
 
 ISR(INT0_vect)
 {
-	if(iLights == 1)
-	{	
-		if(iMode == 1)
+	if (iLights == 1)
+	{
+		if (iMode == 1)
 			iMode = 3;
-		else if(iMode == 3)
+		else if (iMode == 3)
 			iMode = 2;
-		else if(iMode == 2)
-			iMode = 1;	
-		PORTD &= ~(1<<PD0); // off AC
-		PORTD &= ~(1<<PD1); // off DC
+		else if (iMode == 2)
+			iMode = 1;
+		PORTD &= ~(1 << PD0); // off AC
+		PORTD &= ~(1 << PD1); // off DC
 		iResetLights = 1;
 	}
 	sleep = 1;
@@ -227,48 +231,48 @@ ISR(INT0_vect)
 
 ISR(TIMER1_OVF_vect)
 {
-	if(iLights == 1)
-	{	
+	if (iLights == 1)
+	{
 		iLightsCount++;
-		if(iLightsCount > 8000)
+		if (iLightsCount > 8000)
 		{
 			iLights = 0;
 			iLightsCount = 0;
 		}
-		if(iResetLights)
-		{	
+		if (iResetLights)
+		{
 			iLightsCount = 0;
 			iResetLights = 0;
 		}
 	}
 	else
-	{	
+	{
 		iLights = 0;
 		iResetLights = 0;
 		iLightsCount = 0;
 	}
-	
-	if(iBuzzer == 1 && iBuzzerCount < 1000)
+
+	if (iBuzzer == 1 && iBuzzerCount < 1000)
 		PORTD ^= (1 << PD4);
-	else if(iBuzzer == 1 && iBuzzerCount < 3000)
-		PORTD &= ~(1<<PD4);
-	else if(iBuzzerCount > 4000)
+	else if (iBuzzer == 1 && iBuzzerCount < 3000)
+		PORTD &= ~(1 << PD4);
+	else if (iBuzzerCount > 4000)
 		iBuzzerCount = 0;
-	if(iBuzzer == 1)
+	if (iBuzzer == 1)
 		iBuzzerCount++;
 }
 
 ISR(TIMER0_OVF_vect)
 {
 	timer0++;
-	
-	if(timer0>40)	// Temp Messung
-	{	
-		timer0=0;
-		
-		w1_command(READ,NULL);
-		tempH=w1_byte_rd();
-		tempL=w1_byte_rd();
+
+	if (timer0 > 40) // Temp Messung
+	{
+		timer0 = 0;
+
+		w1_command(READ, NULL);
+		tempH = w1_byte_rd();
+		tempL = w1_byte_rd();
 
 		start_meas();
 		sleep = 1;
@@ -280,45 +284,45 @@ void updateLCD(int iLowBat)
 	char lcd_Tist[10], lcd_Tsoll[10], lcd_Volt[10];
 	static int iBlink = 0;
 	// prepare string
-	dtostrf(Tist,2,1,lcd_Tist);
-	dtostrf(Tsoll_old,2,1,lcd_Tsoll);
-	dtostrf(fInVoltage,2,1,lcd_Volt);
-	
-	lcd_clear();			
+	dtostrf(Tist, 2, 1, lcd_Tist);
+	dtostrf(Tsoll_old, 2, 1, lcd_Tsoll);
+	dtostrf(fInVoltage, 2, 1, lcd_Volt);
+
+	lcd_clear();
 	// print current temp
-	lcd_setcursor(0,1);
+	lcd_setcursor(0, 1);
 	lcd_string("Ti=");
 	lcd_string(lcd_Tist);
-				
+
 	// print set temp
-	lcd_setcursor(1,0);
+	lcd_setcursor(1, 0);
 	lcd_string("  Ts=");
 	lcd_string(lcd_Tsoll);
-	
+
 	// print mode
-	lcd_setcursor(0,2);
-	if(iMode == 1)
-	 lcd_string("M:GA");
-	else if(iMode == 2)
-	 lcd_string("M:DC");
-    else
-	 lcd_string("M:AC");
-	 
-	 // print status
-	lcd_setcursor(5,2);
-	if(iIsOn)
+	lcd_setcursor(0, 2);
+	if (iMode == 1)
+		lcd_string("M:GA");
+	else if (iMode == 2)
+		lcd_string("M:DC");
+	else
+		lcd_string("M:AC");
+
+	// print status
+	lcd_setcursor(5, 2);
+	if (iIsOn)
 		lcd_string("S:On");
-    else
+	else
 		lcd_string("S:Off");
-	  
+
 	// print input voltage
-	lcd_setcursor(11,2);
-	if(iLowBat && iMode != 3 && iBlink == 1)
-	{	
+	lcd_setcursor(11, 2);
+	if (iLowBat && iMode != 3 && iBlink == 1)
+	{
 		lcd_string("LOW!!");
 		iBlink = 0;
 	}
-	else if(iMode != 3)
+	else if (iMode != 3)
 	{
 		lcd_string(lcd_Volt);
 		lcd_string("V");
